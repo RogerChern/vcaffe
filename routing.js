21a2244777066715edb4b0dd9f0d4312cd8d58fd
RogerChern <chenyuntao08@gmail.com>
2016-05-08 20:01:27 +0800

    update for gist url

---
+++ routing.js
@@ -0,0 +1,20 @@
+let routes = {
+    "/gist/:gistID": handleGist,
+};
+
+function handleGist(gistID) {
+    let url = "https://api.github.com/gists/" + gistID;
+    $.getJSON(url, parseJSON);
+}
+
+function parseJSON(data) {
+    let files = data.files;
+    let result;
+    for (let fileKey in files) {
+        result = files[fileKey].content;
+    }
+    parseLog(result);
+}
+
+let router = Router(routes);
+router.init();
